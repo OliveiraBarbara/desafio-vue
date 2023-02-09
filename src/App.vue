9bf901aa9f9b5c2000dc7c828f82fbf00804ad3e
Barbara Oliveira <barbara_oliveira@ufms.br>
2023-02-09 19:52:15 -0300

    Pesquisa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,56 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Search from './components/Search.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+      <div>
+        <h1>BUSCANDO POKEMONS</h1>
+      </div>
   </header>
 
   <main>
-    <TheWelcome />
+    <Search />
   </main>
+
+  <footer>
+    <div>
+      <p>Desenvolvido por Bárbara Beatriz Bueno de Oliveira</p>
+      <address>barbara_oliveira@ufms.br</address>
+      <phone>(17)99772-8063</phone>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
-}
+  display: flex;
+  justify-content:space-around;
+  align-items: center;
+} 
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+footer {
+  margin: 0px;
+  background-color: #333D51;
+  color: white;
+  font-size: 1em;
+  font-weight: 600;
+  text-align: center;
+  width: 100%;
+  padding: 10px;
+}*/
 
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+  /* header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
-  }
+  } */
 }
 </style>
---
+++ src/components/Search.vue
@@ -0,0 +1,36 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: ''
+      }
+    },
+    methods: {
+      addComment() {
+        const inputNome = document.getElementById('nome');
+        const url1 = 'https://pokeapi.co/api/v2/pokemon/';
+        const nome1 = inputNome.value;
+        const url = url1 + nome1;
+        fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.clear();
+                console.log(data);
+            })
+            .catch((erro) => {
+                console.error(erro);
+            });
+        this.name = '';
+      }
+    }
+  }
+</script>
+
+<template>
+    <div>
+        <input type="text" id="nome" placeholder="Nome do Pokemon" name="poke" class="form-control" v-model="name">
+        <button v-on:click="addComment" type="submit" class="btn btn-primary">Pesquisar</button>
+    </div>
+</template>
