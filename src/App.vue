7e80010882644d557c6d6c7214f90b2f4359b0df
Barbara Oliveira <barbara_oliveira@ufms.br>
2023-02-11 11:59:07 -0300

    Finalização

--- src/App.vue
+++ src/App.vue
@@ -16,41 +16,19 @@ import Search from './components/Search.vue'
   <footer>
     <div>
       <p>Desenvolvido por Bárbara Beatriz Bueno de Oliveira</p>
-      <address>barbara_oliveira@ufms.br</address>
+      <address>ba.bueno.oliveira@gmail.com</address>
       <phone>(17)99772-8063</phone>
     </div>
   </footer>
 </template>
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-  display: flex;
-  justify-content:space-around;
-  align-items: center;
-} 
-
-footer {
-  margin: 0px;
-  background-color: #333D51;
-  color: white;
-  font-size: 1em;
-  font-weight: 600;
-  text-align: center;
-  width: 100%;
-  padding: 10px;
-}*/
-
 @media (min-width: 1024px) {
   header {
     display: flex;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content:space-around;
+    align-items: center;
+    place-items: center;
   }
-
-  /* header {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  } */
 }
 </style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -8,15 +8,20 @@
     },
     methods: {
       async loadPoke() {
-      const inputNome = document.getElementById('nome');
-      const nome = inputNome.value;
-      const url = `https://pokeapi.co/api/v2/pokemon/${nome}`;
-      const poke = await fetch(url);
-      const pokemon = await poke.json();
-      this.pokeData = pokemon;
-      let img = pokemon.sprites.front_default;
-      document.getElementById('pic').setAttribute('src', img);
-      return pokemon
+        try{
+          const inputNome = document.getElementById('nome');
+          const nome = inputNome.value;
+          const url = `https://pokeapi.co/api/v2/pokemon/${nome}`;
+          const poke = await fetch(url);
+          const pokemon = await poke.json();
+          this.pokeData = pokemon;
+          let img = pokemon.sprites.front_default;
+          document.getElementById('pic').setAttribute('src', img);
+          return pokemon
+        }
+        catch(error){
+          alert("Pokemon não encontrado!");
+        }
         // .then((response) => {
         //   return response.json();
         // })
@@ -44,36 +49,33 @@
 
 <template>
     <div class="form">
-      <h3>Digite o nome do Pokemon que deseja pesquisar!</h3>
+      <h2>Digite o nome do Pokemon que deseja pesquisar!</h2>
       <input type="text" id="nome" placeholder="Nome do Pokemon" name="poke" class="form-control" v-model="name">
       <button v-on:click="loadPoke" type="submit" class="btn btn-primary">Pesquisar</button>
     </div>
 
-    <div class="list" v-if="name.length > 0">
+    <div v-if="name.length > 0">
       <div>
-        <h2>Atributos do Pokemon </h2>
-            <ul>
-              <li class="card">
-                <h3 id="poke">{{ pokeData.name }}</h3>
-                <p id="tipo" class="sub">Tipo: 
-                  <span v-for="(type, key) in pokeData.types" :key="key">{{ type.type.name }}. </span>
-                </p>
-                <p id="hab" class="sub">Habilidades:
-                  <span v-for="(hab, key) in pokeData.abilities" :key="key">{{ hab.ability.name}}. </span>
-                </p>
-                <p id="stats" class="sub">Características:
-                  <span v-for="(carac, key) in pokeData.stats" :key="key">{{ carac.stat.name}} = {{carac.base_stat }}. </span>
-                </p>
-                <p id="peso" class="sub">Peso: {{ pokeData.weight }}</p>
-                <img id="pic" :alt="pokeData.name">
-              </li>
-            </ul> 
-      </div>
-
-      <div class="list-evo">
-        <span> <h2>Evolução do Pokemon</h2></span>
-        <h3 id="evoPoke"></h3>
-        <img id="evoPic">
+        <h2>Pokemon Encontrado!</h2>
+          <ul class="list">
+            <li class="card">
+              <h3 id="poke">{{ pokeData.name }}</h3>
+              <p id="tipo" class="sub"><b>Tipo: </b>
+                <span v-for="(type, key) in pokeData.types" :key="key">{{ type.type.name }}. </span>
+              </p>
+              <p id="peso" class="sub"><b>Peso: </b> {{ pokeData.weight }}.</p>
+              <p id="hab" class="sub"><b>Habilidades: </b>
+                <span v-for="(hab, key) in pokeData.abilities" :key="key">{{ hab.ability.name}}. </span>
+              </p>
+              <p id="stats" class="sub"><b>Estatísticas:</b>
+                <ul class="list-stat">
+                  <li v-for="(carac, key) in pokeData.stats" :key="key" class="stat">{{ carac.stat.name}} = {{carac.base_stat }}</li>
+                </ul>
+                <!-- <span v-for="(carac, key) in pokeData.stats" :key="key">{{ carac.stat.name}} = {{carac.base_stat }}. </span> -->
+              </p>
+              <img id="pic" :alt="pokeData.name">
+            </li>
+          </ul> 
       </div>
     </div>
 </template>
@@ -107,13 +109,21 @@
   display: inline-table;
   align-items: center;
   text-align: center;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  padding-left: 35px;
+  padding-right: 35px;
+}
+
+.list{
+  padding: 0px;
 }
 
 #poke {
 	text-transform: capitalize;
 	margin-bottom: 0px;
   margin-top: 5px;
-	font-size: 32px;
+	font-size: 36px;
 	font-weight: normal;
 }
 
@@ -121,9 +131,25 @@
 	margin: 10px;
 	color: #2b2929;
 	font-weight: normal;
+  font-size: 18px;
+}
+
+.list-stat{
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+.stat{
+  text-transform: capitalize;
+  display: flex;
+  flex-direction: column;
+  padding: 2px;
 }
 
 #pic{
-  height: 120px;
+  height: 130px;
+  background-color: rgba(93, 93, 199, 0.74);
+  border-radius: 50px;
 }
 </style>
